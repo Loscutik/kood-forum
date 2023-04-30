b76256fa16ee1dfa9cfea39b5864c047e605d09d
loscutik <loscutik@gmail.com>
2023-04-30 16:14:26 +0000

    before pulling master2

--- app/wrappers.go
+++ app/wrappers.go
@@ -11,12 +11,32 @@ func checkMethods(r *http.Request, methods ...string) bool {
 	return false
 }
 
-func (app *application) MustMethods (h http.Handler, allowedMethods ...string) http.Handler{
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request)  {
-		if checkMethods(r, allowedMethods...){
-			app.MethodNotAllowed(w, r, http.MethodPost)
+/*
+MustMethods wrapper makes sure that the request's method is allowed
+*/
+func (app *application) MustMethods(h http.Handler, allowedMethods ...string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if checkMethods(r, allowedMethods...) {
+			app.MethodNotAllowed(w, r, allowedMethods...)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) NotAuth(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ses, err := app.checkLoggedin(w, r)
+		if err != nil {
+			// checkLoggedin has already written error status to w
+			return
+		}
+		if ses.LoginStatus == loggedin {
+			w.Header().Add("Location", "/")
+			w.WriteHeader(204)
+			return
 		}
 		
+		h.ServeHTTP(w, r)
 	})
-
 }
